91c229ac21e01f49507053abb84a13e172022e96
Ethanscharlie <ethanscharlie@protonmail.com>
2025-07-30 13:45:41 -0600

    Tweaked to feel better

--- enemyRopeDetector.gd
+++ enemyRopeDetector.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @onready var rope := get_node("../Player/Rope")
 @onready var colShape := get_node("CollisionShape2D")
 
-@export var minSegments = 14
+@export var minSegments = 12
 
 var radius: float
 
@@ -20,9 +20,7 @@ func _physics_process(dt):
 			count += 1
 	
 	if count >= minSegments:
-		print("BANG")
-	else:
-		print("ONG")
+		queue_free()
 	
 func is_point_within_circle(point: Vector2) -> bool:
 	return self.position.distance_to(point) < radius

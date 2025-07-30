c739d62a1a25230619791a5c05823152632f246e
Ethanscharlie <ethanscharlie@protonmail.com>
2025-07-30 14:42:32 -0600

    4 directional movement

--- playerMove.gd
+++ playerMove.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 50
+@export var max_speed = 500
 @export var turn_speed = 5
+@export var drag = 1
 
 func _physics_process(delta):
-	if Input.is_action_pressed("right"):
-		rotation += turn_speed * delta
-
-	if Input.is_action_pressed("left"):
-		rotation -= turn_speed * delta
-
-	var direction = Vector2(1, 0).rotated(rotation)  # Rotate (1, 0) by the current rotation
-	velocity = direction * speed
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity += input_direction * speed
+		
+	velocity = velocity.lerp(Vector2(), drag * delta)
+	velocity.x = clamp(velocity.x, -max_speed, max_speed)
+	velocity.y = clamp(velocity.y, -max_speed, max_speed)
+		
 	move_and_slide()

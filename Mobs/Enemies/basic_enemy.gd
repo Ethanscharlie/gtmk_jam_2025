ce5e0566f898e4a42d7e94a614e47440f58c5a39
Miles Cook <montanaelisabethcook@gmail.com>
2025-07-30 19:38:10 -0400

    spawner added

--- Mobs/Enemies/basic_enemy.gd
+++ Mobs/Enemies/basic_enemy.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
-@export var player: CharacterBody2D
+@onready var player:= get_node("../Player")
 @export var speed = 180
 
 func _physics_process(dt):
---
+++ Mobs/Enemies/enemy_spawner.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@export var basic_enemy: PackedScene
+
+func _ready():
+	_start_timer()
+	
+func _start_timer():
+	while true:
+		await get_tree().create_timer(2).timeout
+		spawn_enemies()
+		
+func spawn_enemies():
+	var enemy = basic_enemy.instantiate()
+	get_parent().add_child(enemy)
+	

b6f73db230c0e47a5887865605da18d925cba023
Ethanscharlie <ethanscharlie@protonmail.com>
2025-07-31 14:12:55 -0600

    Random enemy spawn location

--- Scripts/enemy_spawner.gd
+++ Scripts/enemy_spawner.gd
@@ -3,6 +3,9 @@ extends Node2D
 signal enemy_spawned(new_enemy)
 
 @export var spawn_rate = 2
+@export var spawn_margin = 30
+var SCREEN_WIDTH = 1920
+var SCREEN_HEIGHT = 1080
 
 var basic_enemy = preload("res://Scenes/Entities/basic_enemy.tscn")
 
@@ -12,10 +15,36 @@ func _ready():
 func _start_timer():
 	while true:
 		await get_tree().create_timer(spawn_rate).timeout
-		spawn_enemies()
+		spawn_enemy()
 		
-func spawn_enemies():
+func _get_random_spawn_position() -> Vector2:
+	var picked_position = Vector2(0, 0)
+	
+	var random_percentage = randf()
+	var side = _pick_random_side()
+	match side:
+		"north":
+			picked_position.x = SCREEN_WIDTH * random_percentage
+			picked_position.y = -spawn_margin
+		"south":
+			picked_position.x = SCREEN_WIDTH * random_percentage
+			picked_position.y = SCREEN_HEIGHT + spawn_margin
+		"east":
+			picked_position.x = SCREEN_WIDTH + spawn_margin
+			picked_position.y = SCREEN_HEIGHT * random_percentage
+		"width":
+			picked_position.x = -spawn_margin
+			picked_position.y = SCREEN_HEIGHT * random_percentage
+	
+	return picked_position
+	
+func _pick_random_side() -> String:
+	var sides = ["north", "south", "east", "width"]
+	return sides.pick_random()
+	
+func spawn_enemy():
 	var enemy = basic_enemy.instantiate()
+	enemy.position = _get_random_spawn_position()
 	get_parent().get_parent().add_child(enemy)
 	
 	emit_signal("enemy_spawned", enemy)

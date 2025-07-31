079e8478c0b6e794457307fc9f58c1a63bdca046
Ethanscharlie <ethanscharlie@protonmail.com>
2025-07-31 11:41:49 -0600

    Added the score label and removed lighting

--- Scripts/score_keeper.gd
+++ Scripts/score_keeper.gd
@@ -1,10 +1,18 @@
 extends Node2D
 
+signal score_updated()
+
+var total_enemies_killed = 0
+var score = 0
 
 func _on_enemy_spawner_enemy_spawned(new_enemy: Variant) -> void:
 	var detector = new_enemy.get_node("Detector")
 	detector.enemy_killed.connect(_on_enemy_killed)
-
 	
 func _on_enemy_killed() -> void:
-	print("Enemy Killed")
+	total_enemies_killed += 1
+	_update_score()
+
+func _update_score() -> void:
+	score = total_enemies_killed
+	emit_signal("score_updated")

145727cd52e31ae24957a0ce88f0253225150f49
Golden404 <golden404studios@gmail.com>
2025-08-01 13:55:39 -0400

    MainMenu

--- MainMenu.gd
+++ MainMenu.gd
@@ -2,12 +2,7 @@ extends Node
 var animation_player
 
 func _ready() -> void:
-	animation_player = $AnimationPlayer
-	animation_player.play("Intro")
-func _on_start_pressed() -> void:
-		get_tree().change_scene_to_file("res://")
-
-
-func _on_exit_pressed() -> void:
-	get_tree().quit()
-		
+	pass
+	
+func _on_play_pressed() -> void:
+	pass # Replace with function body.

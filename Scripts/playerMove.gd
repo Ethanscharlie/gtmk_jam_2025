85a0ff982e8f03252ac5b249588f368ee70c812e
Miles Cook <montanaelisabethcook@gmail.com>
2025-08-01 15:39:54 -0400

    fixed dash cus i broke it

--- Scripts/playerMove.gd
+++ Scripts/playerMove.gd
@@ -1,26 +1,48 @@
 extends CharacterBody2D
 
 @export var speed = 50
-@export var max_speed = 500
+
 @export var turn_speed = 5
 @export var drag = 5
 
+@export var dash_speed = 2000
+@export var dash_cooldown = 1
+var can_dash = true
+
+
 signal hit
+signal reset_wave
 
 func _ready():
 	show()
 
 func _physics_process(delta):
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity += input_direction * speed
-		
-	velocity = velocity.lerp(Vector2(), drag * delta)
-	velocity.x = clamp(velocity.x, -max_speed, max_speed)
-	velocity.y = clamp(velocity.y, -max_speed, max_speed)
-		
+	_add_input_to_velocity()
+	_apply_velocity_drag(delta)
+
 	move_and_slide()
+
+func _input(event):
+	if Input.is_action_pressed("dash") and can_dash:
+		_dash()
+		
+func _dash():
+	velocity = _get_input_direction_vector() * dash_speed
+	_queue_dash_cooldown()
+
+func _queue_dash_cooldown():
+	can_dash = false
+	await get_tree().create_timer(dash_cooldown).timeout
+	can_dash = true
 	
+func _add_input_to_velocity():
+	velocity += _get_input_direction_vector() * speed
+	
+func _get_input_direction_vector() -> Vector2:
+	return Input.get_vector("left", "right", "up", "down")
+
+func _apply_velocity_drag(delta):
+	velocity = velocity.lerp(Vector2(), drag * delta)
+
 func _on_hit() -> void:
 	print("player hit")
-	get_tree().paused = true
-	hide()
